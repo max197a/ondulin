2326bf6c42004a056e802b2fd43e82dda472def8
max197a <max197aa@gmail.com>
2018-11-01 02:12:03 +0200

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -335,6 +335,24 @@ $(document).ready(function() {
       midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
     });
 
+    $("[js-popup-video]").magnificPopup({
+      removalDelay: 500, //delay removal by X to allow out-animation
+      disableOn: 700,
+      type: "iframe",
+      callbacks: {
+        beforeOpen: function() {
+          // just a hack that adds mfp-anim class to markup
+          this.st.image.markup = this.st.image.markup.replace(
+            "mfp-figure",
+            "mfp-figure mfp-with-anim"
+          );
+          this.st.mainClass = this.st.el.attr("data-effect");
+        }
+      },
+      closeOnContentClick: true,
+      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
+
     $("[js-popup-image]").magnificPopup({
       type: "image",
       removalDelay: 500, //delay removal by X to allow out-animation
@@ -635,7 +653,7 @@ $(document).ready(function() {
 
       // store overlay globally to access in fadein
       this.$overlay = $(
-        '<div class="js-transition-overlay"><img src="img/logotype.svg" alt="" /></div>'
+        '<div class="js-transition-overlay"><img src="../img/logotype.svg" alt="" /></div>'
       );
       this.$overlay.insertAfter(".header");
       $("body").addClass("is-transitioning");
